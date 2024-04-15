29c2949e41e7a245fa100b8a872019cbed84409e
lpnam <kgzwhite00@gmail.com>
2024-04-16 05:40:24 +0700

    use layout and nested layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "../styles/globals.css";
+import SideBar from "@/ui/SideBar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,7 +18,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <SideBar />
+        <main>{children}</main>
+        <div className="h-screen w-1/6 fixed right-0"></div>
+      </body>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,14 @@
-"use client";
-
 import SideBar from "@/ui/SideBar";
 import Banner from "@/ui/Banner";
 import OutstandingHeader from "@/ui/OutstandingHeader";
 
 export default function Home() {
   return (
-    <div>
-      <SideBar />
+    <main>
       <div className="body-container">
         <Banner />
         <OutstandingHeader />
       </div>
-      <div className="h-screen w-1/6 fixed right-0"></div>
-    </div>
+    </main>
   );
 }
